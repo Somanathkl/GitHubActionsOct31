0c1b80e0f6e5fb6c4c7323343681edd364a9dcfb
Somanathkl <lsomanath@deloitte.com>
2024-09-06 12:23:27 +0530

    Update

--- scripts/Shell/deltaPackage.sh
+++ scripts/Shell/deltaPackage.sh
@@ -6,13 +6,13 @@ fi
 IFS=''
 while read -r line; do
 temp=$(echo $line | grep "objectTranslations")
-if [[ ! -z $temp ]]
+if [ ! -z $temp ];
 then
     flag=1
 else
     flag=0
 fi
-if [[ $flag -eq 1 ]]; then
+if [ $flag -eq 1 ]; then
     newpath=$(echo $line | sed 's/\/[^/]*$//')
     folder=$(echo $line | awk -F/ '{print $(NF-1)}')
     newpath=$newpath"/"$folder".objectTranslation-meta.xml"
